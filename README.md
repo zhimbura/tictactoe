754c5f658b742697bfa7d603c7db0eaa4c2eb011
Устинов Тихон <tih.ustinov2013@yandex.ru>
2022-07-31 19:51:57 +0300

    release ios example

--- README.md
+++ README.md
@@ -838,7 +838,158 @@ xcodebuild -create-xcframework \
 
 ![newiosapp.png](images/newiosapp.png)
 
-Соответственно указываем название приложение и наименование организации
+Соответственно указываем название приложение и наименование организации и выбираем интерфейс **SwiftUI**
 
 ![iosnexttab.png](images/iosnexttab.png)
 
+Далее идем в настройки проекта, для того чтобы подключить созданный нами ранее framework. 
+
+1) Первый шаг кликаем на название проекта, чтобы перейти к его настройкам
+2) Далее кликаем **+** в пункте **Frameworks, Libraries and Embedded content**.
+    В открывшемся окне нажимаем **Add other** и выбираем созданную ранее папку *./core/build/bin/core.xcframework*
+3) Если все сделано правильно наш framework должен появиться в таблице
+
+![add-framework.png](images/add-framework.png)
+
+Теперь при написании кода у наз должен быть доступен для импорта фреймворк с названием соответствующий baseName который мы указали в *./core/build.gradle.kts*. Но давайте эе это проверим и напишем уже реализацию данной игры под ios. Для этого переходим в файл **ContentView** который автоматически создал Xcode при создании проекта. И напишем там вот такой вот код.
+
+*ContentView.swift*
+
+```swift
+import SwiftUI
+// Импортируем наш SDK который мы написали на Kotlin
+import GAMEFramework
+
+// Создаем класс для работы с SDK и чтобы View обновляла данные смотря на него
+class GameUI : ObservableObject {
+    // Переменные которые будут отображаться во view
+    @Published var isGameOver: Bool = false
+    @Published var isGameStarted: Bool = false
+    @Published var winnerName: String = ""
+    @Published var field: [[String]] = [
+        [" ", " ", " "],
+        [" ", " ", " "],
+        [" ", " ", " "]
+    ]
+    // Игра
+    private let game: Game = Game()
+    // Ячейки поля
+    private var gameCells: [String: FieldCell] = [:]
+
+    // По нажатии кнопки играть вызывается этот метод
+    func start () {
+        do {
+            // Подписываемся на события обновления ячеек и окончания игры
+            try game.on(eventType: .changeCell, callBack: onChangeCell)
+            try game.on(eventType: .gameOver, callBack: onGameOver)
+        } catch {
+            print("Ошибка")
+        }
+        // Запускаем процесс игры
+        game.play()
+        isGameStarted = true
+    }
+
+    func playAgain() {
+        isGameOver = false
+        game.reset()
+    }
+
+    // view будет говорить какая кнопка была нажата,
+    // а этот метод будут уже вызывать соответствующий метод у ячеки поля из SDK
+    func clickButton(x: Int, y: Int) {
+        if (isGameOver || !isGameStarted) {
+            return
+        }
+        // Получаем FieldCell и вызываем у него click
+        gameCells[makeHash(x: x, y: y)]?.click()
+    }
+
+    // Обработчик событий изменения состояние ячейки игрового поля
+    private func onChangeCell(event: IEvent) {
+        // Получаем ячеку из события
+        let fieldCell = (event as! GameCellEvent).fieldCell
+        // Создаем хеш и сохраняем в мапу для более удобного поиска в дальнейшем
+        let hash = makeHash(x: Int(fieldCell.getX()), y: Int(fieldCell.getY()))
+        if (!gameCells.keys.contains(hash)) {
+            gameCells[hash] = fieldCell
+        }
+        // и обновляем состояние чтобы view обновлял ячейку которая изменилась
+        field[Int(fieldCell.getY())][Int(fieldCell.getX())] = stateToString(state: fieldCell.getState())
+    }
+
+    // Обработчик окончания игры
+    private func onGameOver(event: IEvent) {
+        // Ставим значения окончания игры чтобы игрок больше не мог кликать кнопки
+        isGameOver = true
+        // И записываем выигравшую сторону
+        switch ((event as! GameOverEvent).winner) {
+        case .zero:
+            winnerName = "O победили"
+        case .cross:
+            winnerName = "X победили"
+        default:
+            winnerName = "Ничья"
+        }
+    }
+
+    // Преобразуем состояние ячеки в строку которая будет выводиться в кнопке
+    private func stateToString(state: PlayerType) -> String {
+        switch state {
+        case .cross:
+            return "X"
+        case .zero:
+            return "O"
+        default:
+            return " "
+        }
+    }
+
+    private func makeHash(x: Int, y: Int) -> String {
+        return String(x) + "-" + String(y)
+    }
+}
+
+// Реализуем отображение
+struct ContentView: View {
+    @ObservedObject var gameUI = GameUI()
+    var body: some View {
+        VStack(alignment: .center) {
+            ForEach((0...2), id: \.self) { y in
+                HStack(spacing: 5) {
+                    Group {
+                        ForEach((0...2), id: \.self) { x  in
+                            Button(action: { gameUI.clickButton(x: x, y: y) }) {
+                                Text(gameUI.field[y][x])
+                            }
+                        }
+                    }
+                    .frame(maxWidth:.infinity)
+                    .padding()
+                    .foregroundColor(Color.black)
+                    .font(.system(size: 64, weight: .bold, design: .default))
+                    .background(Color(red: 0.654, green: 0.654, blue: 0.776))
+                    .cornerRadius(5)
+                }
+            }.opacity(gameUI.isGameStarted ? 1 : 0)
+            Group {
+                Text(gameUI.winnerName)
+                Button("again", action: gameUI.playAgain)
+            }.opacity(gameUI.isGameOver ? 1 : 0)
+            Button("play", action: gameUI.start)
+                .font(.system(size: 32, weight: .bold, design: .default))
+                .opacity(gameUI.isGameStarted ? 0 : 1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+```
+
+Теперь запускаем и получаем вот такой вот результат.
+
+![ios-result](images/ios-result.gif)
--- platforms/swift/tictactoe/tictactoe/ContentView.swift
+++ platforms/swift/tictactoe/tictactoe/ContentView.swift
@@ -1,136 +1,127 @@
-//
-//  ContentView.swift
-//  tictactoe
-//
-//  Created by tikhon on 7/30/22.
-//
-
 import SwiftUI
+// Импортируем наш SDK который мы написали на Kotlin
 import GAMEFramework
 
-struct ContentView: View {
-    @State var buttons: [[String]] = [
+// Создаем класс для работы с SDK и чтобы View обновляла данные смотря на него
+class GameUI : ObservableObject {
+    // Переменные которые будут отображаться во view
+    @Published var isGameOver: Bool = false
+    @Published var isGameStarted: Bool = false
+    @Published var winnerName: String = ""
+    @Published var field: [[String]] = [
         [" ", " ", " "],
         [" ", " ", " "],
         [" ", " ", " "]
     ]
-    @State var isGameOver = false
-    let game = Game()
-    var gameCells: [String: FieldCell] = [:]
-    
-    var body: some View {
-        VStack(alignment: .center) {
-            HStack(spacing: 5) {
-                // TODO переделать на for
-                Group {
-                    Button(action: { clickButton(x: 0, y: 0) }) {
-                        Text(buttons[0][0])
-                    }
-                    Button(action: { clickButton(x: 1, y: 0) }) {
-                        Text(buttons[0][1])
-                    }
-                    Button(action: { clickButton(x: 2, y: 0) }) {
-                        Text(buttons[0][2])
-                    }
-                }
-                .frame(maxWidth:.infinity)
-                .padding()
-                .foregroundColor(Color.black)
-                .font(.system(size: 64, weight: .bold, design: .default))
-                .background(Color(red: 0.654, green: 0.654, blue: 0.776))
-                .cornerRadius(5)
-            }
-            HStack(spacing: 5) {
-                Group {
-                    Button(action: { clickButton(x: 0, y: 1) }) {
-                        Text(buttons[1][0])
-                    }
-                    Button(action: { clickButton(x: 1, y: 1) }) {
-                        Text(buttons[1][1])
-                    }
-                    Button(action: { clickButton(x: 2, y: 1) }) {
-                        Text(buttons[1][2])
-                    }
-                }
-                .frame(maxWidth:.infinity)
-                .padding()
-                .foregroundColor(Color.black)
-                .font(.system(size: 64, weight: .bold, design: .default))
-                .background(Color(red: 0.654, green: 0.654, blue: 0.776))
-                .cornerRadius(5)
-            }
-            HStack(spacing: 5) {
-                Group {
-                    Button(action: { clickButton(x: 0, y: 2) }) {
-                        Text(buttons[2][0])
-                    }
-                    Button(action: { clickButton(x: 1, y: 2) }) {
-                        Text(buttons[2][1])
-                    }
-                    Button(action: { clickButton(x: 2, y: 2) }) {
-                        Text(buttons[2][2])
-                    }
-                }
-                .frame(maxWidth:.infinity)
-                .padding()
-                .foregroundColor(Color.black)
-                .font(.system(size: 64, weight: .bold, design: .default))
-                .background(Color(red: 0.654, green: 0.654, blue: 0.776))
-                .cornerRadius(5)
-            }
-            Group {
-                Text("result")
-                Button("again") {
-                    game.reset()
-                    isGameOver = false
-                }
-            }.opacity(isGameOver ? 1 : 0)
+    // Игра
+    private let game: Game = Game()
+    // Ячейки поля
+    private var gameCells: [String: FieldCell] = [:]
+
+    // По нажатии кнопки играть вызывается этот метод
+    func start () {
+        do {
+            // Подписываемся на события обновления ячеек и окончания игры
+            try game.on(eventType: .changeCell, callBack: onChangeCell)
+            try game.on(eventType: .gameOver, callBack: onGameOver)
+        } catch {
+            print("Ошибка")
         }
+        // Запускаем процесс игры
+        game.play()
+        isGameStarted = true
     }
 
-   
-    init () {
-        do {
-            try game.on(eventType: .changeCell, callBack: {event in
-                print(event)
-            })
-            try game.on(eventType: .gameOver, callBack: {event in
-                print(event)
-            })
-        } catch {
-           print("Ошибка")
-        }
-       
-        game.play()
+    func playAgain() {
+        isGameOver = false
+        game.reset()
     }
-    
-    mutating func onChngeCell(event: IEvent) {
+
+    // view будет говорить какая кнопка была нажата,
+    // а этот метод будут уже вызывать соответствующий метод у ячеки поля из SDK
+    func clickButton(x: Int, y: Int) {
+        if (isGameOver || !isGameStarted) {
+            return
+        }
+        // Получаем FieldCell и вызываем у него click
+        gameCells[makeHash(x: x, y: y)]?.click()
+    }
+
+    // Обработчик событий изменения состояние ячейки игрового поля
+    private func onChangeCell(event: IEvent) {
+        // Получаем ячеку из события
         let fieldCell = (event as! GameCellEvent).fieldCell
-        let hash = String(fieldCell.getX()) + "-" + String(fieldCell.getY())
+        // Создаем хеш и сохраняем в мапу для более удобного поиска в дальнейшем
+        let hash = makeHash(x: Int(fieldCell.getX()), y: Int(fieldCell.getY()))
         if (!gameCells.keys.contains(hash)) {
             gameCells[hash] = fieldCell
         }
-        let x = Int(fieldCell.getX())
-        let y = Int(fieldCell.getY())
-        switch fieldCell.getState() {
-        case .cross:
-            buttons[y][x] = "X"
+        // и обновляем состояние чтобы view обновлял ячейку которая изменилась
+        field[Int(fieldCell.getY())][Int(fieldCell.getX())] = stateToString(state: fieldCell.getState())
+    }
+
+    // Обработчик окончания игры
+    private func onGameOver(event: IEvent) {
+        // Ставим значения окончания игры чтобы игрок больше не мог кликать кнопки
+        isGameOver = true
+        // И записываем выигравшую сторону
+        switch ((event as! GameOverEvent).winner) {
         case .zero:
-            buttons[y][x] = "O"
+            winnerName = "O победили"
+        case .cross:
+            winnerName = "X победили"
         default:
-            buttons[y][x] = " "
+            winnerName = "Ничья"
         }
     }
-    
-    mutating func onGameOver() {
-        isGameOver = true
-        
+
+    // Преобразуем состояние ячеки в строку которая будет выводиться в кнопке
+    private func stateToString(state: PlayerType) -> String {
+        switch state {
+        case .cross:
+            return "X"
+        case .zero:
+            return "O"
+        default:
+            return " "
+        }
     }
-    
-    func clickButton(x: Int, y: Int) {
-        let hash = String(x) + "-" + String(y)
-        print("Click " + hash)
-        gameCells[hash]?.click()
+
+    private func makeHash(x: Int, y: Int) -> String {
+        return String(x) + "-" + String(y)
+    }
+}
+
+// Реализуем отображение
+struct ContentView: View {
+    @ObservedObject var gameUI = GameUI()
+    var body: some View {
+        VStack(alignment: .center) {
+            ForEach((0...2), id: \.self) { y in
+                HStack(spacing: 5) {
+                    Group {
+                        ForEach((0...2), id: \.self) { x  in
+                            Button(action: { gameUI.clickButton(x: x, y: y) }) {
+                                Text(gameUI.field[y][x])
+                            }
+                        }
+                    }
+                    .frame(maxWidth:.infinity)
+                    .padding()
+                    .foregroundColor(Color.black)
+                    .font(.system(size: 64, weight: .bold, design: .default))
+                    .background(Color(red: 0.654, green: 0.654, blue: 0.776))
+                    .cornerRadius(5)
+                }
+            }.opacity(gameUI.isGameStarted ? 1 : 0)
+            Group {
+                Text(gameUI.winnerName)
+                Button("again", action: gameUI.playAgain)
+            }.opacity(gameUI.isGameOver ? 1 : 0)
+            Button("play", action: gameUI.start)
+                .font(.system(size: 32, weight: .bold, design: .default))
+                .opacity(gameUI.isGameStarted ? 0 : 1)
+        }
     }
 }
 
